31a4a546c562a4d11fce1466e6cda7fb40ba82f5
williamHS2024 <williamsantana1701@gmail.com>
2024-06-05 15:17:35 -0600

    integracion de header

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<header class="header">
+        <div class="menu containe">
+            <a href="" class="logo"><img src="./public/build/assets/image/logo.png" alt=""></a>
+            <input type="checkbox" id="menu">
+            <label for="menu">
+                <img src="./public/build/assets/image/menu.png" class="menu-icono" alt="">
+            </label>
+            <nav class="navbar">
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">menu</a></li>
+                    <li><a href="#">menu</a></li>
+                </ul>
+            </nav>
+        </div>
+</header>
