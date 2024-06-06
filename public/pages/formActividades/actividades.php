9b5ba5d043abc386ab19bc9575d3c520df3ad1ec
nicoSg10 <nicolesangocartago@gmail.com>
2024-06-06 16:24:44 -0600

    Modificacion de ruta requiere

--- public/pages/formActividades/actividades.php
+++ public/pages/formActividades/actividades.php
@@ -5,11 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Actividades</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../../build/css/formActividades/actividades.css">
 </head>
 <body>
     
     <header>
-       <?php require("header.php"); ?>
+       <?php require("../../../includes/header.php"); ?>
     </header>
 
     <div class="container mt-5">
@@ -42,7 +43,7 @@
     </div>
 
     <footer>
-        <?php require("header.php"); ?>
+        <?php require("../../../includes/footer.php"); ?>
     </footer>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
