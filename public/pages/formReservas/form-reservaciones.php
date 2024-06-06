bbd2880018a46ab7c110cbcea717a5a73b3bfec6
KendallCR20 <tenancr@gmail.com>
2024-06-06 12:06:28 -0600

    Footer y header(página de formulario de reservas)
    Incluir el header y el footer en la página del formulario de reservas

--- public/pages/formReservas/form-reservaciones.php
+++ public/pages/formReservas/form-reservaciones.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="http://localhost/SalonComunal2024/public/build/css/formReservas/form-reservaciones.css">
+    <link rel="stylesheet" href="http://localhost/SalonComunal2024/public/build/css/header/headerStyle.css">
+    <link rel="stylesheet" href="http://localhost/SalonComunal2024/public/build/css/footer/footerStyle.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
@@ -16,7 +18,7 @@
 <body>
 <header>
 <?php
-require("header.php")
+require("../../../includes/header.php");
 ?>
 </header>
     <div class="container-fluid mt-3">
@@ -204,7 +206,7 @@ require("header.php")
         </div>
         <footer>
         <?php
-        require("footer.php")
+        require("../../../includes/footer.php");
         ?>
         </footer>
 </body>
