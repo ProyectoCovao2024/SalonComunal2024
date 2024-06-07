1ff4e3897da342c0f896fb493bdec3e78de56d35
nicoSg10 <nicolesangocartago@gmail.com>
2024-06-06 19:57:43 -0600

    Footer
    Color blanco al copyright

--- public/pages/formActividades/actividades.php
+++ public/pages/formActividades/actividades.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="http://localhost/SalonComunal2024/public/build/css/formActividades/actividades.css">
     <link rel="stylesheet" href="http://localhost/SalonComunal2024/public/build/css/header/headerStyle.css">
     <link rel="stylesheet" href="http://localhost/SalonComunal2024/public/build/css/footer/footerStyle.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
