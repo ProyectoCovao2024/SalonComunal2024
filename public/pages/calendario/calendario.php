22d62196e31f35bfb9c1ca0349ff8cb61e895e2b
williamHS2024 <williamsantana1701@gmail.com>
2024-06-14 22:10:25 -0600

    creacion calendario

---
+++ public/pages/calendario/calendario.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="http://localhost/SalonComunal2024/public/build/css/calendario/calendario.css">
+    <link rel="stylesheet" href="http://localhost/SalonComunal2024/public/build/css/calendario/dycalendar.css">
+    <script defer src="http://localhost/SalonComunal2024/public/build/JavaScript/dycalendar.js"></script>
+    <title>Calendario</title>
+</head>
+<body>
+    <section>
+        <div class="box">
+            <div class="container">
+                <div id="dycalendar">
+                    <h1>Hola</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="http://localhost/SalonComunal2024/public/build/JavaScript/dycalendar.js"></script>
+    <script>
+        dycalendar.draw({
+            target: "#dycalendar",
+            type: "month",
+            highlighttargetdate: true,
+            prevnextbutton: "show"
+        });
+    </script>
+</body>
+</html>
