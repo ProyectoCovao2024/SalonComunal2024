73e435733da12ede0798af16bad69e6c85f2aadf
williamHS2024 <williamsantana1701@gmail.com>
2024-06-20 22:07:01 -0600

    creacion de la galeria

---
+++ view/pages/galeria/galeria.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/gsap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/zepto/1.2.0/zepto.min.js"></script>
+    <link rel="stylesheet" href="http://localhost/SalonComunal2024/public/build/css/footer/footerStyle.css">
+    <link rel="stylesheet" href="http://localhost/SalonComunal2024/public/build/css/header/headerStyle.css">
+    <link rel="stylesheet" href="http://localhost/SalonComunal2024/public/build/css/galeria/galeria.css">
+    <script defer src="http://localhost/SalonComunal2024/public/build/JavaScript/galeria.js"></script>
+    <title>Document</title>
+</head>
+
+<body >
+<?php
+    require ('../../../includes/header.php')
+?>
+    <div class="sliderImage">
+        <h1>Salon Comunal</h1>
+        <div class="container">
+            <div class="ring">
+                <div class="img"></div>
+                <div class="img"></div>
+                <div class="img"></div>
+                <div class="img"></div>
+                <div class="img"></div>
+                <div class="img"></div>
+                <div class="img"></div>
+                <div class="img"></div>
+                <div class="img"></div>
+                <div class="img"></div>
+            </div>
+        </div>
+        <h1>Exterior</h1>
+        <div class="container">
+            <div class="ring">
+                <div class="img"></div>
+                <div class="img"></div>
+                <div class="img"></div>
+                <div class="img"></div>
+                <div class="img"></div>
+                <div class="img"></div>
+                <div class="img"></div>
+                <div class="img"></div>
+                <div class="img"></div>
+                <div class="img"></div>
+            </div>
+        </div>
+        <h1>Interior</h1>
+        <div class="container">
+            <div class="ring">
+                <div class="img"></div>
+                <div class="img"></div>
+                <div class="img"></div>
+                <div class="img"></div>
+                <div class="img"></div>
+                <div class="img"></div>
+                <div class="img"></div>
+                <div class="img"></div>
+                <div class="img"></div>
+                <div class="img"></div>
+            </div>
+        </div>
+    </div>   
+    <footer>
+        <?php
+        require ('../../../includes/footer.php')
+            ?>
+    </footer>
+</body>
+
+</html>
