95bcb8d8cf9ec523f550a19af77cdae350ad8ba1
nicoSg10 <nicolesangocartago@gmail.com>
2024-06-21 19:08:15 -0600

    update

--- includes/header.php
+++ includes/header.php
@@ -1,16 +1,16 @@
 
 <header class="header">
         <div class="menu">
-            <a href="" class="logo"><img src="http://localhost/SalonComunal2024/public/build/assets/image/logo.png" alt=""></a>
+            <a href="\SalonComunal2024\public\index.php" class="logo"><img src="http://localhost/SalonComunal2024/public/build/assets/image/logo.png" alt=""></a>
             <input type="checkbox" id="menu">
             <label for="menu">
                 <img src="http://localhost/SalonComunal2024/public/build/assets/image/menu.png" id="img" class="menu-icono" alt="">
             </label>
             <nav class="navbar">
                 <ul>
-                    <li><a href="#">Inicio</a></li>
-                    <li><a href="#">menú</a></li>
-                    <li><a href="#">menú</a></li>
+                    <li><a href="\SalonComunal2024\view\pages\formReservas\form-reservaciones.php">Reservaciones</a></li>
+                    <li><a href="\SalonComunal2024\view\pages\calendario\calendario.php">Calendario</a></li>
+                    <li><a href="\SalonComunal2024\view\pages\login\login.php">Iniciar sesión</a></li>
                 </ul>
             </nav>
         </div>
--- view/pages/calendario/calendario.php
+++ view/pages/calendario/calendario.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="http://localhost/SalonComunal2024/public/build/assets/image/logo.ico">
     <link rel="stylesheet" href="http://localhost/SalonComunal2024/public/build/css/calendario/calendario.css">
     <link rel="stylesheet" href="http://localhost/SalonComunal2024/public/build/css/calendario/dycalendar.css">
     <link rel="stylesheet" href="http://localhost/SalonComunal2024/public/build/css/header/headerStyle.css">
