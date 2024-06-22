0e4c9f69f9248d3cf41766a98219e10a4e0cefab
williamHS2024 <williamsantana1701@gmail.com>
2024-06-21 21:29:02 -0600

    update

--- view/pages/postIngreso/postIngreso.php
+++ view/pages/postIngreso/postIngreso.php
@@ -10,16 +10,7 @@ require ("../../../includes/url.php");
     <link rel="stylesheet" href="<?php echo urlLocal?>/public/build/css/header/headerStyle.css">
     <link rel="stylesheet" href="<?php echo urlLocal?>/public/build/css/footer/footerStyle.css">
     <link rel="stylesheet" href="<?php echo urlLocal?>/public/build/css/postIngreso/postIngreso.css">
-    <title>Document</title>
-</head>
-<body>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css">
-    <title>Document</title>
+    <title>Post Ingreso</title>
 </head>
 <body>
 <?php
