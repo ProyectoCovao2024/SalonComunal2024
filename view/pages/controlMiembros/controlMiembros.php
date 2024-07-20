26d4ac226ef5c1430b9b5a65e0c6a2444fc81a20
Kendall Andrés Orozco Gómez <kendallandres0803@gmail.com>
2024-07-19 21:07:40 -0600

    Pagina Miembros actualizado!
    Se actualizó la pagina Control de Miembros! (Aún en proceso por pequeñas cosas)

--- view/pages/controlMiembros/controlMiembros.php
+++ view/pages/controlMiembros/controlMiembros.php
@@ -12,6 +12,7 @@ require ("../../../includes/url.php");
     <link rel="stylesheet" href="<?php echo urlLocal?>/public/build/css/header/headerStyle.css">
     <link rel="icon" type="image/x-icon" href="<?php echo urlLocal?>/public/build/assets/image/logo.ico">
     <link rel="stylesheet" href="<?php echo urlLocal?>/public/build/css/footer/footerStyle.css">
+    <link rel="stylesheet" href="<?php echo urlLocal?>/public/build/css/formMiembros/miembrstyle.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
@@ -20,17 +21,6 @@ require ("../../../includes/url.php");
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <title>Control de miembros</title>
 </head>
-
-
-
-
-
-
-
-
-
-
-
 <body>
     <header>
         <?php
@@ -38,6 +28,85 @@ require ("../../../includes/url.php");
         ?>
     </header>
     
+
+    <h1 class="text-center p-3">Control de Miembros</h1>
+
+    <div class="container-fluid row">
+        <form id="crudForm" class="col-4 p-3 styleform" method="POST">
+            <h3 class="text-center alert alert-secondary">Formulario</h3>
+            <?php
+            // include("modelo/conexion.php");
+            // include("controlador/registro_auto.php");
+            // include("controlador/eliminar_fulldatos.php");
+            ?>
+            <div class="mb-3">
+                <label for="marca" class="form-label">Nombre</label>
+                <input type="text" class="form-control" name="marca">
+            </div>
+            <div class="mb-3">
+                <label for="modelo" class="form-label">Cédula</label>
+                <input type="number" class="form-control" name="modelo">
+            </div>
+            <div class="mb-3">
+                <label for="capacidadmotor" class="form-label">Dirección</label>
+                <input type="number" class="form-control" name="capacidadmotor">
+            </div>
+            <div class="mb-3">
+                <label for="transmision" class="form-label">Celular</label>
+                <input type="text" class="form-control" name="transmision">
+            </div>
+            <div class="mb-3">
+                <label for="traccion" class="form-label">Correo</label>
+                <input type="text" class="form-control" name="traccion">
+            </div>
+            <button type="submit" class="btn btn-primary" name="btnregistro" value="ok">Registrar</button>
+            <button type="button" class="btn btn-secondary" onclick="limpiarCampos()">Limpiar Campos</button>
+            <div id="multiActionButton" class="d-none">
+                    <button onclick="return fulleliminar()" type="submit" name="btneliminar" class="btn btn-danger">Eliminar Todos los Datos Seleccionados</button>
+            </div>
+        </form>
+
+        <div class="col-8 p-4">
+            <?php
+            // include("controlador/eliminar_auto.php");
+            ?>
+            <form method="POST">
+                <table class="table">
+                    <thead class="table-info">
+                        <tr>
+                            <th scope="col"><input type="checkbox" onclick="seleccionarTodos(this)"></th>
+                            <th scope="col">ID</th>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Cédula</th>
+                            <th scope="col">Dirección</th>
+                            <th scope="col">Celular</th>
+                            <th scope="col">Correo</th>
+                            <th scope="col">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        <tr>
+                        <td><input type="checkbox" name="selectAuto[]"  onclick="toggleActionButtons()"></td>
+                        <td>1</td>
+                        <td>Example</td>
+                        <td>000000000</td>
+                        <td>Example</td>
+                        <td>99999999</td>
+                        <td>Example@gmial</td>
+                        <td>        
+                            <a class="btn btn-small btn-warning"><i class='bx bx-edit'></i></a>
+                            <a onclick="return eliminar()" class="btn btn-small btn-danger"><i class='bx bxs-trash'></i></a>
+                        </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </form>
+        </div>
+    </div>
+
+
+
     <footer>
         <?php
         require("../../../includes/footer.php");
