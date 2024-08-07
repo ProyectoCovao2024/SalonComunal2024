5435901794ddfcc74dff0b15aa6611e1894911d3
KendallCR20 <tenancr@gmail.com>
2024-08-06 18:54:50 -0600

    Update

--- controller/formularioActividades.php
+++ controller/formularioActividades.php
@@ -1,5 +1,5 @@
 <?php
-require_once(__DIR__ . "/../model/formularioActividades.php");
+require_once(_DIR_ . "/../model/formularioActividades.php");
 $formularioActividad = new formularioActividades_model();
 
 $listaActividades = $formularioActividad->getformularioActividades();
@@ -13,37 +13,35 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $tipoActividad = isset($_POST['nombre_actividad']) ? $_POST['nombre_actividad'] : null;
     $codigoTipodeMonetizacion = isset($_POST['tipR']) ? $_POST['tipR'] : null;
     $accion = isset($_POST['action']) ? $_POST['action'] : null;
+    $tipoActividadAntiguo = isset($_POST['tipo_actividad_antiguo']) ? $_POST['tipo_actividad_antiguo'] : null;
 
     $tipoActividad = trim(ltrim(rtrim($tipoActividad)));
+    $tipoActividadAntiguo = trim(ltrim(rtrim($tipoActividadAntiguo)));
 
-    // Verifica que los campos no estén vacíos para la acción 'add'
     if ($accion === 'add' && (!empty($tipoActividad) && !empty($codigoTipodeMonetizacion) && $tipoActividad != '')) {
         try {
-            if ($accion === 'add') {
-                // Verifica si la actividad ya existe
-                if ($formularioActividad->actividadExiste($tipoActividad)) {
+            if ($formularioActividad->actividadExiste($tipoActividad)) {
+                header('Refresh: 3; url=http://localhost/SalonComunal2024/view/pages/formActividades/actividades.php');
+                echo '
+                    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+                    <script type="text/javascript">
+                        window.onload = function(){
+                            Swal.fire({
+                                icon: "error",
+                                title: "Ya existe una actividad con ese nombre!",
+                                showConfirmButton: false
+                            });
+                        }
+                    </script>
+                ';
+                exit();      
+            } else {
+                $resultado = $formularioActividad->crearActividad($tipoActividad, $codigoTipodeMonetizacion);
+                if ($resultado) {
                     header('Refresh: 3; url=http://localhost/SalonComunal2024/view/pages/formActividades/actividades.php');
-                    echo'
+                    echo '
                         <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-                        <script type="text/Javascript">
-                            window.onload = function(){
-                                Swal.fire({
-                                    icon: "error",
-                                    title: "Ya existe una actividad con ese nombre!",
-                                    showConfirmButton: false
-                                });
-                            }
-                        </script>
-                    ';
-                    exit();          
-                } else {
-                    // Llama al método para crear la actividad en el modelo
-                    $resultado = $formularioActividad->crearActividad($tipoActividad, $codigoTipodeMonetizacion);
-                    if ($resultado) {
-                        header('Refresh: 3; url=http://localhost/SalonComunal2024/view/pages/formActividades/actividades.php');
-                        echo'
-                        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-                        <script type="text/Javascript">
+                        <script type="text/javascript">
                             window.onload = function(){
                                 Swal.fire({
                                     icon: "success",
@@ -53,15 +51,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                             }
                         </script>
                     ';
-                    }
-                }
-            } elseif ($accion === 'delete') {
-                // Llama al método para eliminar la actividad en el modelo
-                $resultado = $formularioActividad->eliminarActividad($tipoActividad);
-                if ($resultado === true) {
-                    echo "Actividad eliminada con éxito.";
-                } else {
-                    echo $resultado; // Mostrar el mensaje de que la actividad no existe
                 }
             }
         } catch (Exception $e) {
@@ -69,21 +58,65 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
     } elseif ($accion === 'delete' && !empty($tipoActividad) && $tipoActividad != '') {
         try {
-            // Llama al método para eliminar la actividad en el modelo
             $resultado = $formularioActividad->eliminarActividad($tipoActividad);
             if ($resultado === true) {
-                echo "Actividad eliminada con éxito.";
+                header('Refresh: 3; url=http://localhost/SalonComunal2024/view/pages/formActividades/actividades.php');
+                echo '
+                    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+                    <script type="text/javascript">
+                        window.onload = function(){
+                            Swal.fire({
+                                icon: "success",
+                                title: "Actividad eliminada con éxito!",
+                                showConfirmButton: false
+                            });
+                        }
+                    </script>
+                ';
             } else {
-                echo $resultado; // Mostrar el mensaje de que la actividad no existe
+                header('Refresh: 3; url=http://localhost/SalonComunal2024/view/pages/formActividades/actividades.php');
+                echo '
+                    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+                    <script type="text/javascript">
+                        window.onload = function(){
+                            Swal.fire({
+                                icon: "error",
+                                title: "Actividad no encontrada!",
+                                showConfirmButton: false
+                            });
+                        }
+                    </script>
+                ';
+            }
+        } catch (Exception $e) {
+            echo "Error al procesar la solicitud: " . $e->getMessage();
+        }
+    } elseif ($accion === 'update' && !empty($tipoActividad) && !empty($codigoTipodeMonetizacion) && !empty($tipoActividadAntiguo)) {
+        try {
+            $resultado = $formularioActividad->actualizarActividad($tipoActividadAntiguo, $tipoActividad, $codigoTipodeMonetizacion);
+            if ($resultado) {
+                header('Refresh: 3; url=http://localhost/SalonComunal2024/view/pages/formActividades/actividades.php');
+                echo '
+                    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+                    <script type="text/javascript">
+                        window.onload = function(){
+                            Swal.fire({
+                                icon: "success",
+                                title: "Actividad actualizada con éxito!",
+                                showConfirmButton: false
+                            });
+                        }
+                    </script>
+                ';
             }
         } catch (Exception $e) {
             echo "Error al procesar la solicitud: " . $e->getMessage();
         }
     } else {
         header('Refresh: 3; url=http://localhost/SalonComunal2024/view/pages/formActividades/actividades.php');
-        echo'
+        echo '
             <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-            <script type="text/Javascript">
+            <script type="text/javascript">
                 window.onload = function(){
                     Swal.fire({
                         icon: "error",
@@ -91,10 +124,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                         showConfirmButton: false
                     });
                 }
-            
             </script>
         ';
         exit();
-    }
+    }
 }
+
 ?>
